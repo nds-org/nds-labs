8f8194e4e550a880d0b8576e0136e5f3a4a9693b
Kacper Kowalik <xarthisius.kk@gmail.com>
2014-10-30 13:10:45 -0500

    Add launcher for ytwebapp

---
+++ docker-launcher/launch-ytwebapp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+RABBIT_HOST=rabbit
+RABBIT_IMAGE=rabbit:latest
+CELERY_IMAGE=ytwebapp:latest
+
+RABBIT_CID=$(docker run -d --name $RABBIT_HOST $RABBIT_IMAGE)
+RABBIT_AUTH=$(docker logs ${RABBIT_CID} | awk '/curl/ {print $3}')
+BROKER_URL=amqp://${RABBIT_AUTH}@amq:${AMQ_PORT_5672_TCP_PORT}
+CELERY_CID=$(docker run -d -v /var/lib/docker:/var/lib/docker --privileged -p 8888 --link $RABBIT_HOST:amq -e BROKER_URL=${BROKER_URL} ${CELERY_IMAGE})
