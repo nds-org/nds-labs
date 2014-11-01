b73209b36bfdaa9ebaf247caecef4cb600b9e32c
Kacper Kowalik <xarthisius.kk@gmail.com>
2014-10-31 19:38:51 -0500

    update launchers

--- docker-launcher/launch-ytwebapp.sh
+++ docker-launcher/launch-ytwebapp.sh
@@ -6,4 +6,5 @@ CELERY_IMAGE=ndslabs/ythub_worker:latest
 RABBIT_CID=$(docker run -d --name $RABBIT_HOST $RABBIT_IMAGE)
 RABBIT_AUTH=$(docker logs ${RABBIT_CID} | awk '/curl/ {print $3}')
 BROKER_URL=amqp://${RABBIT_AUTH}@amq:${AMQ_PORT_5672_TCP_PORT}
-CELERY_CID=$(docker run -d -v /var/lib/docker:/var/lib/docker --privileged -p 8888 --link $RABBIT_HOST:amq -e BROKER_URL=${BROKER_URL} ${CELERY_IMAGE})
+CELERY_CID=$(docker run -d -v /var/lib/docker:/var/lib/docker --privileged -p 8888 --link $RABBIT_HOST:amq --link
+${ICAT_NAME}:${ICAT_NAME} -e BROKER_URL=${BROKER_URL} ${CELERY_IMAGE})
