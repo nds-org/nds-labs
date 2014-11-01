e3d2d3e9bd6cd5e3d5e8a712883a1e1790f16850
Kacper Kowalik <xarthisius.kk@gmail.com>
2014-10-31 19:36:54 -0500

    Register /mnt/data into the irods server, create ytfido user

--- irods/icat/setup-irods-docker.sh
+++ irods/icat/setup-irods-docker.sh
@@ -23,6 +23,17 @@ EOF
 
 sudo su -c "/var/lib/irods/packaging/setup_irods.sh </home/admin/dbresp"
 sudo usermod -G admin -a irods
-#change irods user's irodsEnv file to point to localhost, since it was configured with a transient Docker container's hostname
-sed -i 's/^irodsHost.*/irodsHost localhost/' /var/lib/irods/.irods/.irodsEnv
+## change irods user's irodsEnv file to point to localhost, since 
+## it was configured with a transient Docker container's hostname
+# sed -i 's/^irodsHost.*/irodsHost localhost/' /var/lib/irods/.irods/.irodsEnv
+## I reverted that ^^ I doesn't seem to be required ~xarth
+
+sleep 2
+sudo -u irods -i iadmin modresc demoResc host $HOSTNAME
+sudo -u irods -i iadmin mkuser ytfido rodsuser
+sudo -u irods -i iadmin moduser ytfido password ${ytfidopassword}
+sudo -u irods -i imkdir /${irodszone}/home/rods/data
+sudo -u irods -i imcoll -m filesystem /mnt/data /${irodszone}/home/rods/data
+sudo -u irods -i ichmod -r read ytfido /${irodszone}/home/rods/data
+
 /usr/bin/supervisord "-n"
