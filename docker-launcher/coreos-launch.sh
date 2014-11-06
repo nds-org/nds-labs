06a3903282f6c36b86325c670a0d12fa22592bcd
Matthew Turk <matthewturk@gmail.com>
2014-11-06 17:34:06 -0500

    Updating image and removing config.

--- docker-launcher/coreos-launch.sh
+++ docker-launcher/coreos-launch.sh
@@ -2,10 +2,9 @@
 
 nova boot \
 --user-data ./coreos-config.yaml \
---image 3a03810f-2ede-4f9e-8766-3b574388b2df \
+--image fd4d996e-9cf4-42bc-a834-741627b0e499 \
 --key-name bb \
 --flavor 3 \
 --num-instances 3 \
 --security-groups default coreos \
---config-drive=true \
 --nic net-id=165265ee-d257-43d7-b3b7-e579cd749ed4
