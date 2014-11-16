a42eb1bfbb5be84593868114bd9de34c4161216a
Kacper Kowalik <xarthisius.kk@gmail.com>
2014-11-15 19:07:09 -0600

    Add latest Doockerfile for hublaunch

---
+++ hublaunch/setup_irods.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import etcd
+import subprocess
+import urlparse
+import json
+from pwd import getpwnam
+
+user = getpwnam("user")
+uhome = user.pw_dir
+uuid = user.pw_uid
+ugid = user.pw_gid
+ihome = os.path.join(uhome, ".irods")
+if not os.path.isdir(ihome):
+    os.mkdir(ihome, 0700)
+    # os.chown(ihome, uuid, ugid)
+
+if not os.path.isfile(os.path.join(ihome, ".irodsEnv")):
+    host_ip = os.environ.get('COREOS_PRIVATE_IPV4', None)
+    client = etcd.Client(host=host_ip, port=4001)
+    with open(os.path.join(ihome, ".irodsEnv"), 'w') as fh:
+        fh.write("irodsHost %s\n" % client.read('/irods/host').value)
+        fh.write("irodsPort %s\n" % client.read('/irods/port').value)
+        fh.write("irodsZone %s\n" % client.read('/irods/zone').value)
+        fh.write("irodsUserName ytfido\n")
+    # os.chown(os.path.join(ihome, ".irodsEnv"), uuid, ugid)
+
+if not os.path.isfile(os.path.join(ihome, ".irodsA")):
+    cmd = "iinit %s" % os.environ.get('ytfidopassword', '3nthr0py')
+    subprocess.call(cmd, shell=True)
+
+# Mount iRODS resources
+cwd = os.getcwd()
+for directory in json.loads(os.environ.get('mounts', '[]')):
+    os.chdir("/mnt/data")
+    path = urlparse.urlparse(directory).path
+    target = os.path.join("/mnt/data", os.path.basename(path))
+    os.mkdir(target)
+    cmd = 'icd ' + path + ' && ' + 'irodsFs -o allow_other ' + target
+    subprocess.call(cmd, shell=True)
+
+# os.chdir("fuse")
+os.execlp("python2.7", "-u", sys.argv[1])
+os.chdir(cwd)
