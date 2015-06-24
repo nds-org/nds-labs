d164411a7958a72c364c7a90c890a7123dc86192
Mike Freemon <mfreemon@illinois.edu>
2015-06-23 23:03:57 -0500

    describe solution in comments to the vlad management network problem

--- run_startup.sh
+++ run_startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# deal with VLAD networking
+# iptables -A OUTPUT -t nat -p tcp -d 10.10.236.1 -j DNAT --to 127.0.0.1
+# ssh -L localhost:5000:localhost:5000 -L localhost:8774:localhost:8774 -4 -nNT vlad-mgmt
+
 # image-id coreos 681  a8839020-cb32-46c7-b0d4-882c5315dc22
 # net-id nds_net       165265ee-d257-43d7-b3b7-e579cd749ed4
 # flavor-id s1.medium  02a02a3c-d0b4-4bd1-9933-bb4391ad10b2
