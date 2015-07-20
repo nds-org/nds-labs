4139804875cbeb4d2e8a3ff3cc89a37e60f36eec
Mike Freemon <mfreemon@illinois.edu>
2015-07-20 17:41:35 -0500

    minor run startup script adjustments

--- run_startup.sh
+++ run_startup.sh
@@ -8,18 +8,20 @@
 # net-id nds_net       165265ee-d257-43d7-b3b7-e579cd749ed4
 # flavor-id s1.medium  02a02a3c-d0b4-4bd1-9933-bb4391ad10b2
 
+# ssh-key-name is the name (as known by OpenStack) of the key that was added to OpenStack
+
 source NDS-openrc.sh
 
 python startup_ndslabs.py \
- --dbvol-id 8e28fba9-2742-4964-a6a0-d3afa5ec0404 \
- --icatvol-id bc31ee6a-c383-4a3e-821a-87ed534e9501 \
- --moinmoin-id 56d469a7-ea71-4d5c-8590-aa829f8eb92a \
+ --dbvol-id ac747c89-4e9c-46e1-8c10-e95af4b49d51 \
+ --icatvol-id d8bdc318-789c-463e-83f9-811e97c615c3 \
+ --moinmoin-id 51ce47c2-9920-4964-83e1-b5427ae4f96e \
  --ip 141.142.204.184 \
  --total-vms 3 \
  --total-public 1 \
  --name mfreemon \
- --ssh-key /home/mfreemon/PycharmProjects/nds-labs/mfreemon-key.pub \
- --env-file /home/mfreemon/PycharmProjects/nds-labs/docker-launcher/production.env \
+ --ssh-key $PWD/mfreemon-at-shark-key.pub \
+ --env-file $PWD/docker-launcher/production.env \
  --region NCSA \
  --net-id 165265ee-d257-43d7-b3b7-e579cd749ed4 \
  --image-id a8839020-cb32-46c7-b0d4-882c5315dc22 \
