5cda3b230fc7c9e4254a7ff94be8c1517537c35a
Michael J Lambert <lambert8@lambert8.ncsa.illinois.edu>
2016-02-11 16:36:33 -0600

    tweaks to parameterize the tool server

---
+++ services/terra/clowdertools/set-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# set -e
+
+# Clowder URI
+export CLOWDER_ADDR=10.0.0.57
+export CLOWDER_PORT=9000
+
+# Tool Server URI
+export TOOLSRV_ADDR=10.0.0.120
+export TOOLSRV_PORT=8082
+
+# Request metadata
+export TOOLSRV_UNAME=lambert8@illinois.edu
+export TOOLSRV_PW=testtest
+export TOOLSRV_TARGET_DATASET=56ba103be4b01696e6f1cd07
+
+echo "${TOOLSRV_UNAME}:******** is requesting dataset ${TOOLSRV_TARGET_DATASET}"
+echo "${CLOWDER_ADDR}:${CLOWDER_PORT} <-> ${TOOLSRV_ADDR}:${TOOLSRV_PORT}"
+echo ""
